d945d857b0e8efbdd53ad6792e88d47ece52813f
fivepost <fivepost@yandex.ru>
2022-04-12 15:34:15 +0500

    movielist fix

--- components/MovieList.jsx
+++ components/MovieList.jsx
@@ -9,7 +9,7 @@ const MovieList = ({ movies }) => {
 		<div className="bg-zinc-100 dark:bg-zinc-700 ">
 			<div className="container mx-auto p-2">
 				<h1 className='mt-8 mb-5 text-2xl color-black dark:color-white'>{t('common:popular_movies')}</h1>
-				<div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 ">
+				<div className="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 ">
 					{movies.map(movie => (
 						<MovieCard movie={movie} key={movie.id} />
 					))}
