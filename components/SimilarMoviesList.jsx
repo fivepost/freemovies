a019670b41146fdadb6aaffd80648e6f49e72c45
fivepost <fivepost@yandex.ru>
2022-04-12 15:28:56 +0500

    Мелкие правки

--- components/SimilarMoviesList.jsx
+++ components/SimilarMoviesList.jsx
@@ -9,11 +9,11 @@ import useTransition from "next-translate/useTranslation"
 
 
 const SimilarMoviesList = ({ movies }) => {
-	const { t } = useTransition()
+	const { t } = useTransition('common')
 
 	return (
 		<div className="container mx-auto px-2 mb-3">
-			<h1 className='mt-8 mb-5 text-2xl color-black dark:color-white'>{t('common:similar_movies')}</h1>
+			<h1 className='mt-8 mb-5 text-2xl color-black dark:color-white'>{t('similar_movies')}</h1>
 			<Swiper
 				spaceBetween={20}
 				slidesPerView={1.2}
@@ -27,23 +27,23 @@ const SimilarMoviesList = ({ movies }) => {
 				modules={[ Pagination, Autoplay]}
 				breakpoints={{
 					320: {
-						slidesPerView: 1.2,
+						slidesPerView: 2.2,
 						spaceBetween: 12
 					},
 					480: {
-						slidesPerView: 2.2,
+						slidesPerView: 3.2,
 						spaceBetween: 15
 					},
 					640: {
-						slidesPerView: 3.2,
-						spaceBetween: 20
-					},
-					768: {
 						slidesPerView: 4.2,
 						spaceBetween: 20
 					},
-					1024: {
+					768: {
 						slidesPerView: 5.2,
+						spaceBetween: 20
+					},
+					1024: {
+						slidesPerView: 6.2,
 						spaceBetween: 20
 					},
 					1280: {
