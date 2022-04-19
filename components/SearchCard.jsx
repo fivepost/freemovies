a1b31400121abf19fbaf22dfc36d8d617928f5c0
fivepost <fivepost@yandex.ru>
2022-04-19 22:19:46 +0500

    some fix

--- components/SearchCard.jsx
+++ components/SearchCard.jsx
@@ -5,13 +5,14 @@ import TextTruncate from 'react-text-truncate'
 const SearchCard = () => {
 	return (
 		<div className="bg-white rounded-md grid grid-cols-6 overflow-hidden dark:bg-zinc-800 dark:text-white text-gray-600 dark:text-white">
-			<div className="col-span-2 h-full">
-				<Image className="w-full object-cover "
+			<div className="col-span-2 relative">
+				<Image 
 					src='/mockposter.webp'
 					alt="River"
 					width="350"
 					height="525"
-					layout='responsive'
+					layout="fill"
+					objectFit="cover"
 					placeholder="blur"
 					blurDataURL="/blur-placeholder2.jpg" />
 			</div>
--- components/SearchModal.jsx
+++ components/SearchModal.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { motion, AnimatePresence } from 'framer-motion'
 import { useEffect } from 'react';
 import { IoMdClose } from "react-icons/io";
@@ -33,29 +34,31 @@ const SearchModal = ({ showModal, setShowModal }) => {
 					initial="hidden"
 					exit="hidden"
 				>
-					<div className="container overflow-y-auto no-scrollbar  h-full p-2" onClick={(e) => e.stopPropagation()} >
-						<input placeholder="Type some movie..." type="text" className="my-7 w-full border border-slate-300 focus:border-slate-400  p-2 sm:p-3 rounded-md bg-transparent text-white" />
-						<div className="grid grid-cols-1  lg:grid-cols-2 xl:grid-cols-3 gap-2 sm:gap-3">
-							<SearchCard/>
-							<SearchCard/>
-							<SearchCard/>
-							<SearchCard/>
-							<SearchCard/>
-							<SearchCard/>
-							<SearchCard/>
-							<SearchCard/>
-							<SearchCard/>
-							<SearchCard/>
-							<SearchCard/>
-							<SearchCard/>
-							<SearchCard/>
+					<div className="container   h-full p-2" onClick={(e) => e.stopPropagation()} >
+						<input autoFocus={true} placeholder="Type some movie..." type="text" className="mt-10 mb-3 sticky top-8 z-40 	  w-full border border-slate-300 focus:border-slate-400  p-2 sm:p-3 rounded-md bg-transparent text-white"  />
+						<div className="overflow-y-scroll no-scrollbar pt-4 h-full">
+						<div className="grid grid-cols-1  lg:grid-cols-2 xl:grid-cols-3 gap-3 sm:gap-4">
+							<SearchCard />
+							<SearchCard />
+							<SearchCard />
+							<SearchCard />
+							<SearchCard />
+							<SearchCard />
+							<SearchCard />
+							<SearchCard />
+							<SearchCard />
+							<SearchCard />
+							<SearchCard />
+							<SearchCard />
+							<SearchCard />
+						</div>
 						</div>
 					</div>
 					<button
 						onClick={closeModal}
 						className="fixed top-0 right-0 text-white/60 p-3 bg-black/40"
 					> <IoMdClose size={16} /></button>
-			
+
 				</motion.div>
 
 			}
